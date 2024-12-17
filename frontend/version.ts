4a0d20329a8118e5cb726d6c53b42b8dc374c884
Lucio Tarantino <lucio.tarantino@gmail.com>
2024-12-17 21:21:28 +0000

    chore(webapp): :bookmark: Mark version

--- frontend/version.ts
+++ frontend/version.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import packageJson from "./package.json";
 import semver from "semver";
 import { parseArgs } from "util";
+import { $ } from "bun";
 
 const { values, positionals } = parseArgs({
     args: Bun.argv,
@@ -15,17 +16,28 @@ const { values, positionals } = parseArgs({
             type: 'string',
             short: 'i'
         },
+        overwite: {
+            type: 'string',
+            short: 'o'
+        },
     },
     strict: true,
     allowPositionals: true
 });
 //const currentVersion = semver.parse(packageJson.version);
-let newVersion = semver.inc(packageJson.version, values.releaseType as semver.ReleaseType, values.identifier);
+let newVersion: string | null = packageJson.version;
+if (values.overwite && values.overwite?.trim() !== 'TEST_INTERNAL') {
+    newVersion = values.overwite;
+} else {
+    newVersion = semver.inc(packageJson.version, values.releaseType as semver.ReleaseType, values.identifier);
+}
 
 if (newVersion && newVersion !== packageJson.version) {
     packageJson.version = newVersion;
     fs.writeFileSync("./package.json", JSON.stringify(packageJson, null, 2));
     console.log(`Version updated to ${newVersion}`);
+    //await $`git add ./package.json`;
+    //await $`git commit -m "chore(webapp): :bookmark: Mark frontend version ${newVersion}"`;
 } else {
     console.log("No version update required");
 }
