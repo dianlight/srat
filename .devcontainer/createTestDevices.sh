cf67f1c75ea5eeba22f1150320f89769ce92a302
dianlight <lucio.tarantino@gmail.com>
2024-12-30 10:09:28 +0000

    fix(devcontainer): :bug: Update updateContentCommand structure and fix script path in createTestDevices.sh

--- .devcontainer/createTestDevices.sh
+++ .devcontainer/createTestDevices.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo "----------------------------------------------------------------"
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "$SCRIPT_DIR"
 losetup -a
-find $(SCRIPT_DIR)/../backend/test/data/ -name "*.dmg" -exec losetup -P -f '{}' \;
+find ${SCRIPT_DIR}/../backend/test/data/ -name "*.dmg" -exec losetup -P -f '{}' \;
 mdev -s
