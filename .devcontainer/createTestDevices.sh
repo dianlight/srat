f6713325b7253c75effdb4e3a313254b570377e7
dianlight <lucio.tarantino@gmail.com>
2024-12-30 10:23:31 +0000

    fix(devcontainer): :bug: Update createTestDevices.sh to suppress output of mdev command

--- .devcontainer/createTestDevices.sh
+++ .devcontainer/createTestDevices.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "$SCRIPT_DIR"
 losetup -a
 find ${SCRIPT_DIR}/../backend/test/data/ -name "*.dmg" -exec losetup -P -f '{}' \;
-mdev -s
+mdev -s |:
