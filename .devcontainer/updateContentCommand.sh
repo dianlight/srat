c966b2af8f776bcd4e5d8871da31c416e4163698
dianlight <lucio.tarantino@gmail.com>
2025-01-02 18:59:09 +0000

    feat(devcontainer): :sparkles: Add gotest installation to updateContentCommand.sh and build workflow

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -9,3 +9,4 @@ curl -fsSL https://bun.sh/install | bash
 #curl -s -L https://github.com/swaggo/swag/releases/download/v2.0.0-rc4/swag_2.0.0-rc4_Linux_arm64.tar.gz | tar xzvf - -C /usr/local/bin
 GOBIN=/usr/local/bin/ go install github.com/swaggo/swag/v2/cmd/swag@latest
 GOBIN=/usr/local/bin/ go install github.com/rogpeppe/gohack@latest
+GOBIN=/usr/local/bin/ go install github.com/rakyll/gotest@latest
