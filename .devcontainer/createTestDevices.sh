30774edef21d19ef8b3ab6eb48aed6986a5a91e3
Lucio Tarantino <lucio.tarantino@gmail.com>
2025-01-21 13:20:46 +0000

    fix(devcontainer): handle mdev command output for improved error handling in createTestDevices.sh

--- .devcontainer/createTestDevices.sh
+++ .devcontainer/createTestDevices.sh
@@ -3,6 +3,6 @@ echo "----------------------------------------------------------------"
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 echo "$SCRIPT_DIR"
 losetup -a
-find ${SCRIPT_DIR}/../backend/test/data/ -name "*.dmg" -exec losetup -P -f '{}' \; -exec sleep 1 \; 
-#mdev -s >/dev/null ||:
-losetup -a | grep .devcontainer
+find ${SCRIPT_DIR}/../backend/test/data/ -name "*.dmg" -exec losetup -P -f '{}' \; -exec sleep 1 \;  ||:
+mdev -s 2>/dev/null ||:
+losetup -a | grep .devcontainer 
