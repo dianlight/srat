6a2de40c5c0ed3ade20bdd3a22b7090d4d736d8d
dianlight <lucio.tarantino@gmail.com>
2025-03-04 22:48:08 +0000

    ♻️ Refactor: Update system handlers to use fuego context and improve response handling

--- backend/src/api/system.go
+++ backend/src/api/system.go
@@ -4,13 +4,13 @@ import (
 	"bufio"
 	"io"
 	"log/slog"
-	"net/http"
 	"os"
 	"strings"
 
 	"github.com/dianlight/srat/converter"
 	"github.com/dianlight/srat/dto"
 	"github.com/go-fuego/fuego"
+	"github.com/go-fuego/fuego/option"
 	"github.com/jaypipes/ghw"
 	"github.com/jpillora/overseer"
 )
@@ -25,54 +25,32 @@ func NewSystemHanler() *SystemHanler {
 }
 
 func (p *SystemHanler) Routers(srv *fuego.Server) error {
-	fuego.PutStd(srv, "/restart", p.RestartHandler)
-	fuego.GetStd(srv, "/nics", p.GetNICsHandler)
-	fuego.GetStd(srv, "/filesystems", p.GetFSHandler)
+	fuego.Put(srv, "/restart", p.RestartHandler, option.Description("Restart the server ( useful in development )"), option.Tags("dev"))
+	fuego.Get(srv, "/nics", p.GetNICsHandler, option.Description("Return all network interfaces"), option.Tags("system"))
+	fuego.Get(srv, "/filesystems", p.GetFSHandler, option.Description("Return all supported fs"), option.Tags("system"))
 	return nil
 }
 
-// RestartHandler godoc
-//
-//	@Summary		RestartHandler
-//	@Description	Restart the server ( useful in development )
-//	@Tags			system
-//	@Produce		json
-//	@Success		204
-//	@Failure		405	{object}	dto.ErrorInfo
-//	@Router			/restart [put]
-func (handler *SystemHanler) RestartHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNoContent)
-
+func (handler *SystemHanler) RestartHandler(c fuego.ContextNoBody) (bool, error) {
 	slog.Debug("Restarting server...")
 	overseer.Restart()
+	return true, nil
 }
 
-// GetNICsHandler godoc
-//
-//	@Summary		GetNICsHandler
-//	@Description	Return all network interfaces
-//	@Tags			system
-//	@Produce		json
-//	@Success		200 {object}	dto.NetworkInfo
-//	@Failure		405	{object}	dto.ErrorInfo
-//	@Router			/nics [get]
-func (handler *SystemHanler) GetNICsHandler(w http.ResponseWriter, r *http.Request) {
+func (handler *SystemHanler) GetNICsHandler(c fuego.ContextNoBody) (*dto.NetworkInfo, error) {
 
 	net, err := ghw.Network()
 	if err != nil {
-		HttpJSONReponse(w, err, nil)
-		return
+		return nil, err
 	}
 
 	var info dto.NetworkInfo
 	var conv converter.NetToDtoImpl
 	err = conv.NetInfoToNetworkInfo(*net, &info)
-	//err = mapper.Map(context.Background(), &info, net)
 	if err != nil {
-		HttpJSONReponse(w, err, nil)
-		return
+		return nil, err
 	}
-	HttpJSONReponse(w, info, nil)
+	return &info, nil
 }
 
 // ReadLinesOffsetN reads contents from file and splits them by new line.
@@ -131,30 +109,15 @@ func (handler *SystemHanler) getFileSystems() ([]string, error) {
 	return ret, nil
 }
 
-// GetFSHandler godoc
-//
-//	@Summary		GetFSHandler
-//	@Description	Return all supported fs
-//	@Tags			system
-//	@Produce		json
-//	@Success		200 {object}	dto.FilesystemTypes
-//	@Failure		405	{object}	dto.ErrorInfo
-//	@Router			/filesystems [get]
-func (handler *SystemHanler) GetFSHandler(w http.ResponseWriter, r *http.Request) {
+func (handler *SystemHanler) GetFSHandler(c fuego.ContextNoBody) (dto.FilesystemTypes, error) {
 
 	fs, err := handler.getFileSystems()
 	if err != nil {
-		HttpJSONReponse(w, err, nil)
-		return
+		return nil, err
 	}
 	var xfs dto.FilesystemTypes
 	for _, fsi := range fs {
 		xfs = append(xfs, dto.FilesystemType(fsi))
 	}
-	//err = mapper.Map(context.Background(), &xfs, fs)
-	//if err != nil {
-	//	HttpJSONReponse(w, err, nil)
-	//	return
-	//}
-	HttpJSONReponse(w, xfs, nil)
+	return xfs, nil
 }
--- backend/src/api/system_test.go
+++ backend/src/api/system_test.go
@@ -1,12 +1,10 @@
 package api_test
 
 import (
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/dianlight/srat/api"
+	"github.com/go-fuego/fuego"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -29,53 +27,23 @@ func TestSystemHandlerSuite(t *testing.T) {
 }
 
 func (suite *SystemHandlerSuite) TestGetNICsHandler() {
-	api := api.NewSystemHanler()
-	req, err := http.NewRequestWithContext(testContext, "GET", "/nics", nil)
-	suite.Require().NoError(err)
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(api.GetNICsHandler)
-
-	handler.ServeHTTP(rr, req)
-
-	suite.Equal(http.StatusOK, rr.Code, "Expected status code 200, got %d", rr.Code)
-
-	expectedContentType := "application/json"
-	suite.Equal(expectedContentType, rr.Header().Get("Content-Type"), "Expected content type %s, got %s", expectedContentType, rr.Header().Get("Content-Type"))
-
-	var response map[string]interface{}
-	err = json.Unmarshal(rr.Body.Bytes(), &response)
+	ctx := fuego.NewMockContextNoBody()
+	response, err := api.NewSystemHanler().GetNICsHandler(ctx)
 	suite.Require().NoError(err)
 	suite.T().Logf("%v", response)
 
-	if nics, ok := response["nics"].([]interface{}); !ok || len(nics) == 0 {
+	if len(response.NICs) == 0 {
 		suite.T().Errorf("Response does not contain any network interfaces")
 	}
 }
 
 func (suite *SystemHandlerSuite) TestGetFSHandler() {
-	api := api.NewSystemHanler()
-	req, err := http.NewRequestWithContext(testContext, "GET", "/filesystems", nil)
+	ctx := fuego.NewMockContextNoBody()
+	response, err := api.NewSystemHanler().GetFSHandler(ctx)
 	suite.Require().NoError(err)
+	suite.T().Logf("%v", response)
 
-	// Create a ResponseRecorder to record the response
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(api.GetFSHandler)
-
-	// Call the handler
-	handler.ServeHTTP(rr, req)
-
-	// Check the status code
-	suite.Equal(http.StatusOK, rr.Code, "Expected status code 200, got %d", rr.Code)
-
-	// Check the content type
-	expectedContentType := "application/json"
-	suite.Equal(expectedContentType, rr.Header().Get("Content-Type"), "Expected content type %s, got %s", expectedContentType, rr.Header().Get("Content-Type"))
-
-	// Check the response body
-	var fileSystems []string
-	err = json.Unmarshal(rr.Body.Bytes(), &fileSystems)
-	suite.Require().NoError(err)
-	suite.T().Logf("%v", fileSystems)
-	suite.NotEmpty(fileSystems, "Response does not contain any file systems")
+	if len(response) == 0 {
+		suite.T().Errorf("Response does not contain any file systems")
+	}
 }
