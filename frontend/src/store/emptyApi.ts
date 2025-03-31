9b1db85c515d11bda7dbe58355b2e6bd8c37494a
dianlight <lucio.tarantino@gmail.com>
2025-03-31 21:04:15 +0000

    fix: Enhance API URL handling; add reachability check and default fallback for APIURL

--- frontend/src/store/emptyApi.ts
+++ frontend/src/store/emptyApi.ts
@@ -2,11 +2,39 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 let APIURL = process.env.APIURL;
-if (process.env.APIURL === "dynamic") {
+async function testURL(url: string): Promise<boolean> {
+    try {
+        const parsedUrl = new URL(url);
+        return await fetch(url, { method: 'GET' })
+            .then((response) => {
+                if (response.ok) {
+                    console.log(`API URL is reachable: ${url}`);
+                    return true;
+                } else {
+                    console.error(`API URL is not reachable: ${url}`);
+                    return false;
+                }
+            })
+            .catch((error) => {
+                console.error(`Error fetching API URL: ${error}`);
+                return false;
+            });
+    } catch (e) {
+        return false;
+    }
+}
+
+
+// test if APIURL is set and if is reaceable
+if (APIURL === undefined || APIURL === "") {
+    console.error("APIURL is not set, using default: http://localhost:8080");
+    APIURL = "http://localhost:8080";
+} else if (process.env.APIURL === "dynamic" || !(await testURL(APIURL))) {
     APIURL = window.location.href.substring(0, window.location.href.lastIndexOf('/'));
     console.info(`Dynamic APIURL provided, using generated: ${APIURL}/ from ${window.location.href}`)
 }
-console.log("* API URL", APIURL + "/");
+console.log("* API URL", APIURL + "/", "Reachable: ", await testURL(APIURL));
+
 // initialize an empty api service that we'll inject endpoints into later as needed
 export const emptySplitApi = createApi({
     baseQuery: fetchBaseQuery({
