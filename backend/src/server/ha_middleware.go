383919112a27466b7a636f55ca58cef136be7029
dianlight <lucio.tarantino@gmail.com>
2025-04-01 20:09:31 +0000

    fix: Update OpenAPI schema for event handling; rename events and adjust data references in health and share events

--- backend/src/server/ha_middleware.go
+++ backend/src/server/ha_middleware.go
@@ -2,36 +2,62 @@ package server
 
 import (
 	"context"
-	"log"
+	"log/slog"
 	"net/http"
-	"os"
+	"strings"
+
+	"github.com/dianlight/srat/homeassistant/ingress"
 )
 
-// HAMiddleware is a middleware function for handling HomeAssistant authentication.
-// It checks for the presence and validity of the X-Supervisor-Token in the request header.
+var ingressClient *ingress.ClientWithResponses
+
+// HAMiddleware is a middleware function that ensures requests are coming from
+// an authorized Home Assistant environment. It performs the following checks:
+//
+//  1. Verifies the presence of the "X-Remote-User-Id" header in the request.
+//     If the header is missing, it logs an error and responds with an HTTP 401 Unauthorized status.
+//
+//  2. Validates the remote IP address of the request against a predefined list
+//     of allowed IPs. If the IP is not in the allowed list, it logs an error
+//     and responds with an HTTP 401 Unauthorized status.
+//
+// If both checks pass, the middleware adds the "user_id" from the header to
+// the request context and forwards the request to the next handler in the chain.
 //
 // Parameters:
-//   - next: The next http.Handler in the chain to be called if authentication is successful.
+// - next: The next http.Handler to be called if the request passes the middleware checks.
 //
 // Returns:
-//   - http.Handler: A new http.Handler that wraps the authentication logic around the next handler.
-//     If authentication fails, it returns a 401 Unauthorized status.
-//     If successful, it adds the token to the request context and calls the next handler.
+// - An http.Handler that wraps the provided handler with the middleware logic.
 func HAMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		tokenString := r.Header.Get("X-Supervisor-Token")
-		if tokenString == "" {
-			log.Printf("Not in a HomeAssistant environment! %#v", r.Header)
+		allowedIPs := []string{"172.30.32.2", "127.0.0.1"}
+		user_id := r.Header.Get("X-Remote-User-Id")
+		if user_id == "" {
+			slog.Error("Not in a HomeAssistant environment!", "header", r.Header)
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
+		remoteIP := r.RemoteAddr
+		// Remove port if present
+		ip := strings.Split(remoteIP, ":")[0]
 
-		if tokenString != os.Getenv("SUPERVISOR_TOKEN") {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
+		allowed := false
+		for _, allowedIP := range allowedIPs {
+			if ip == allowedIP {
+				allowed = true
+				break
+			}
 		}
 
-		ctx := context.WithValue(r.Context(), "auth_token", tokenString)
+		if !allowed {
+			slog.Error("Unauthorized access from", "IP", ip)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "user_id", user_id)
 		next.ServeHTTP(w, r.WithContext(ctx))
+		return
+
 	})
 }
