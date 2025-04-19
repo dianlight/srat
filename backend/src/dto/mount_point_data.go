5ea400a91c47cd683cd0907fe43c2fdddf1b6042
dianlight <lucio.tarantino@gmail.com>
2025-04-19 15:13:08 +0000

    ♻️ refactor: remove unused fields from MountPointData struct

--- backend/src/dto/mount_point_data.go
+++ backend/src/dto/mount_point_data.go
@@ -1,17 +1,12 @@
 package dto
 
 type MountPointData struct {
-	// DeviceId Unique and persistent id for filesystem.
-	//ID string `json:"id"`
-	// Path Mount point path.
-	Path string `json:"path"`
-	//PrimaryPath string   `json:"primary_path,omitempty"`
-	FSType string   `json:"fstype,omitempty"`
-	Flags  []string `json:"flags,omitempty" enum:"MS_RDONLY,MS_NOSUID,MS_NODEV,MS_NOEXEC,MS_SYNCHRONOUS,MS_REMOUNT,MS_MANDLOCK,MS_NOATIME,MS_NODIRATIME,MS_BIND,MS_LAZYTIME,MS_NOUSER,MS_RELATIME"`
-	// Source Device source of the filesystem (e.g. /dev/sda1).
-	Device       string  `json:"device,omitempty"`
-	IsMounted    bool    `json:"is_mounted,omitempty"`
-	IsInvalid    bool    `json:"invalid,omitempty"`
-	InvalidError *string `json:"invalid_error,omitempty"`
-	Warnings     *string `json:"warnings,omitempty"`
+	Path         string   `json:"path"`
+	FSType       string   `json:"fstype,omitempty"`
+	Flags        []string `json:"flags,omitempty" enum:"MS_RDONLY,MS_NOSUID,MS_NODEV,MS_NOEXEC,MS_SYNCHRONOUS,MS_REMOUNT,MS_MANDLOCK,MS_NOATIME,MS_NODIRATIME,MS_BIND,MS_LAZYTIME,MS_NOUSER,MS_RELATIME"`
+	Device       string   `json:"device,omitempty"` // Source Device source of the filesystem (e.g. /dev/sda1).
+	IsMounted    bool     `json:"is_mounted,omitempty"`
+	IsInvalid    bool     `json:"invalid,omitempty"`
+	InvalidError *string  `json:"invalid_error,omitempty"`
+	Warnings     *string  `json:"warnings,omitempty"`
 }
