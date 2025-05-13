0f029c89a0d90135bc5011f080685393d077d4c5
dianlight <lucio.tarantino@gmail.com>
2025-05-13 21:49:23 +0000

    ⬆️ upgrade base image and bun version

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -3,7 +3,7 @@
 apk add --no-cache git make lsblk eudev gcc musl-dev linux-headers samba ethtool e2fsprogs e2fsprogs-extra fuse3 exfatprogs ntfs-3g-progs apfs-fuse openssh-client sshfs
 apk add --no-cache --update-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community  go "go=1.24.2-r1" 
 #bun
-curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.10" 
+curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.13" 
 
 #GOBIN=/usr/local/bin/ go install github.com/rogpeppe/gohack@v1.0.2
 #GOBIN=/usr/local/bin/ go install github.com/rakyll/gotest@v0.0.6
