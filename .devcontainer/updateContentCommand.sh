673c0c81f9c11f7a389cabe47161b2ec039b665f
dianlight <lucio.tarantino@gmail.com>
2025-06-19 20:46:15 +0000

    🔧 chore(devcontainer): Update devcontainer configuration and dependencies

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -e
+set -x
 
 apk add --no-cache git make lsblk eudev gcc musl-dev linux-headers samba ethtool e2fsprogs e2fsprogs-extra fuse3 exfatprogs ntfs-3g-progs apfs-fuse openssh-client sshfs pre-commit shadow
 apk add --no-cache --update-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community go "go~=1.24"
@@ -9,6 +9,6 @@ curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.15"
 make -C .. prepare || :
 
 cd "$(dirname "$0")/.."
-apk add --no-cache py3-pip
-python3 -m pip install ruff  openapi-python-client
+apk add --no-cache py3-pip libturbojpeg ffmpeg go2rtc libpcap-dev
+python3 -m pip install ruff openapi-python-client
 openapi-python-client --install-completion
