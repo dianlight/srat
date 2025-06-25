c96660b68ce55780cb23826111b6a48061fb2c3d
dianlight <lucio.tarantino@gmail.com>
2025-06-25 20:31:01 +0000

    ✨ feat(devcontainer): Install Gemini CLI and add project configuration guide

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -4,5 +4,11 @@ apk add --no-cache git make lsblk eudev gcc musl-dev linux-headers samba ethtool
 apk add --no-cache --update-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community go "go~=1.24"
 #bun
 curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.15"
+ln -s /.bun/bin/bun /usr/local/bin/node
 
 make -C .. prepare || :
+
+#gemini
+bun add -g @google/gemini-cli
+bun pm -g untrusted
+bun pm -g trust
