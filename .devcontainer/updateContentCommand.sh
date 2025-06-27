7595459bca9cbb07dd7547c8e27633224cfca0c1
dianlight <lucio.tarantino@gmail.com>
2025-06-27 13:41:30 +0000

    🔧 chore(devcontainer): Add set -x for debugging and fix gemini cli install

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 
 apk add --no-cache git make lsblk eudev gcc musl-dev linux-headers samba ethtool e2fsprogs e2fsprogs-extra fuse3 exfatprogs ntfs-3g-progs apfs-fuse openssh-client sshfs pre-commit shadow go
 #apk add --no-cache --update-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community go "go~=1.24"
@@ -9,6 +10,5 @@ ln -s /.bun/bin/bun /usr/local/bin/node
 make -C .. prepare || :
 
 #gemini
-bun add -g @google/gemini-cli
-bun pm -g untrusted
-bun pm -g trust
+bun add -g @google/gemini-cli ||:
+bun pm -g trust --all ||:
