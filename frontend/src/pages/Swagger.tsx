bf6ab59c2ca0a13241df720e8d5522cc225c4d75
dianlight <lucio.tarantino@gmail.com>
2025-08-12 15:37:18 +0000

    ✨ feat(Swagger): refactor `<openapi-explorer>` component usage
    * Simplified the usage of the `<openapi-explorer>` component by declaring it as a constant.
    * Removed unnecessary JSX declarations and comments for cleaner code.

--- frontend/src/pages/Swagger.tsx
+++ frontend/src/pages/Swagger.tsx
@@ -3,32 +3,16 @@ import 'openapi-explorer';
 import { apiUrl } from "../store/emptyApi";
 
 // Allow the custom web component <openapi-explorer> in TSX
-declare global {
-	namespace JSX {
-		interface IntrinsicElements {
-			'openapi-explorer': any;
-		}
-	}
-}
+const OpenApiExplorer = 'openapi-explorer' as any;
 
 export function Swagger() {
-	/*
-		return <InView as="div">
-			<SwaggerUI url={apiUrl + "openapi-3.0.json"} />
-		</InView>
-		*/
-
 	return (
 		<Box>
-			<>
-				<openapi-explorer spec-url={`${apiUrl}/openapi.json`}>
-					<div slot="overview">
-						<h1>The API</h1>
-					</div>
-				</openapi-explorer>
-				<a href="https://petstore.swagger.io/">Swagger Petstore</a>
-				<p>{apiUrl}/openapi.json</p>
-			</>
+			<OpenApiExplorer spec-url={`${apiUrl}/openapi.json`}>
+				<div slot="overview">
+					<h1>The API</h1>
+				</div>
+			</OpenApiExplorer>
 		</Box>
 	);
 }
