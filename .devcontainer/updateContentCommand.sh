18af3eb58701147fc61552385071e9f5a4021913
dianlight <lucio.tarantino@gmail.com>
2025-10-26 16:38:33 +0000

    🔧 chore(be): update `updateContentCommand.sh` and `Makefile`
    - Added `patch` utility to dependencies in `updateContentCommand.sh`
    - Updated `Makefile` to check for `patch` command availability
    - Modified build step to include `format` in `Makefile`

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -3,7 +3,7 @@ set -x
 
 apk add --no-cache git make lsblk eudev gcc musl-dev linux-headers samba ethtool e2fsprogs e2fsprogs-extra \
  fuse3 exfatprogs ntfs-3g-progs apfs-fuse openssh-client sshfs pre-commit shadow go \
- git-bash-completion git-prompt graphviz nix
+ git-bash-completion git-prompt graphviz nix patch
 apk add --no-cache --update-cache --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community go "go~=1.25"
 #bun
 curl -fsSL https://bun.sh/install | bash -s "bun-v1.3.1"
