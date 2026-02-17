fbff1b852b010de743479244ddaa9ddf616d5a16
dianlight <lucio.tarantino@gmail.com>
2026-02-17 21:10:22 +0000

    🔧 chore: update biome installation command and settings
    - Modify `updateContentCommand.sh` to specify biome CLI path
    - Add biome LSP binary path to VSCode settings

--- .devcontainer/updateContentCommand.sh
+++ .devcontainer/updateContentCommand.sh
@@ -36,7 +36,7 @@ make -C .. prepare || :
 bun install -g @j178/prek ||:
 
 #biome
-bun add -g biome ||:
+bun add -g biome @biomejs/cli-linux-arm64-musl ||:
 bun pm -g trust --all ||:
 #sed -i '1s/node/bun/' "$(realpath $HOME/.bun/bin/biome)" ||:
 #export BIOME_BINARY=$(realpath $HOME/.bun/bin/biome)
